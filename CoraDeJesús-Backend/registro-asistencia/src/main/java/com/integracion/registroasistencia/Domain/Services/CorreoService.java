8a48e7eabe212d19fe1ac3f04d58f0d9cf880293
Pixgel <pixgel13040@gmail.com>
2022-12-13 00:49:35 -0500

    correos config

--- CoraDeJesús-Backend/registro-asistencia/src/main/java/com/integracion/registroasistencia/Domain/Services/CorreoService.java
+++ CoraDeJesús-Backend/registro-asistencia/src/main/java/com/integracion/registroasistencia/Domain/Services/CorreoService.java
@@ -1,4 +1,16 @@
 package com.integracion.registroasistencia.Domain.Services;
 
+import org.springframework.mail.javamail.JavaMailSender;
+
 public class CorreoService {
+
+    private JavaMailSender mailSender;
+
+    public CorreoService(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
+    public void Menzaje_assitencia(){
+
+    }
 }
