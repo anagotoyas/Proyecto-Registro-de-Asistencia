2d6b4ba241d3f92cc9e56becb6c81c318de2b49a
Camila Rivera Calvo <camila.arc91@gmail.com>
2022-12-20 23:45:16 -0500

    aaa

--- CoraDeJesús-Frontend/src/app/docente/shared/docente.service.ts
+++ CoraDeJesús-Frontend/src/app/docente/shared/docente.service.ts
@@ -10,23 +10,5 @@ export class DocenteService {
 
   constructor(private http: HttpClient) { }
 
-  create(resena: Resena) {
-    return this.http.post(`${this.apiBase}/resenas`,resena );
-  }
-
-  getResenaPorIdHistoria(id: number){
-    return this.http.get<Resena[]>(`${this.apiBase}/resenas/buscarPorIdHistoria?historia=${id}`)
-  }
-
-  deleteResena(idResena: number) {
-    return this.http.delete<Resena>(`${this.apiBase}/resenas/${idResena}`)
-  }
-
-  editResena(resena: Resena) {
-    return this.http.put(`${this.apiBase}/resenas`,resena );
-  }
-
-  getResenaPorId(id: number){
-    return this.http.get<Resena>(`${this.apiBase}/resenas/${id}`)
-  }
+  
 }
